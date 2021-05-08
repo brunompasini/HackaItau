adeabb092314de49aad0e4bcb2bf490bf04ffb2c
feteti <feteti@gmail.com>
2021-05-08 10:51:52 -0300

    atualizção

--- Hacka/modulo/metrics.py
+++ Hacka/modulo/metrics.py
@@ -33,4 +33,67 @@ def sharpe_ratio(w, e_r, cov, r_f=0):
     Returns:
         float: Sharpe Ratio do portfólio
     """
-    return (portfolio_return(w, e_r)-r_f)/portfolio_vol(w, cov)   
+    return (portfolio_return(w, e_r)-r_f)/portfolio_vol(w, cov)
+
+
+
+
+class CovarianceEstimator:
+    """
+    Classe usada para estimar a matriz de covariância a partir da matriz estimada pela amostra e pela matriz de correlação constante.
+    Baseado no artigo "Honey, I Shrunk the Sample Covariance Matrix" do Olivier Ledoit e Michael Wolf, The Journal of Portfolio Management.
+    Pegamos boa parte do código da biblioteca PyPortfolioOpt.
+    """
+    def __init__(self, returns):
+        self.returns = returns.dropna()
+        self.S = returns.cov().values
+    
+    def honey_shrinkage(self):
+        """
+        Pegamos da biblioteca pyPortfolioopt.
+        """
+        
+        
+        X = np.nan_to_num(self.returns.values)
+        t, n = np.shape(X)
+
+        S = self.S  # sample cov matrix
+
+        # Constant correlation target
+        var = np.diag(S).reshape(-1, 1)
+        std = np.sqrt(var)
+        _var = np.tile(var, (n,))
+        _std = np.tile(std, (n,))
+        r_bar = (np.sum(S / (_std * _std.T)) - n) / (n * (n - 1))
+        F = r_bar * (_std * _std.T)
+        F[np.eye(n) == 1] = var.reshape(-1)
+
+        # Estimate pi
+        Xm = X - X.mean(axis=0)
+        y = Xm ** 2
+        pi_mat = np.dot(y.T, y) / t - 2 * np.dot(Xm.T, Xm) * S / t + S ** 2
+        pi_hat = np.sum(pi_mat)
+
+        # Theta matrix, expanded term by term
+        term1 = np.dot((Xm ** 3).T, Xm) / t
+        help_ = np.dot(Xm.T, Xm) / t
+        help_diag = np.diag(help_)
+        term2 = np.tile(help_diag, (n, 1)).T * S
+        term3 = help_ * _var
+        term4 = _var * S
+        theta_mat = term1 - term2 - term3 + term4
+        theta_mat[np.eye(n) == 1] = np.zeros(n)
+        rho_hat = sum(np.diag(pi_mat)) + r_bar * np.sum(
+            np.dot((1 / std), std.T) * theta_mat
+        )
+
+        # Estimate gamma
+        gamma_hat = np.linalg.norm(S - F, "fro") ** 2
+
+        # Compute shrinkage constant
+        kappa_hat = (pi_hat - rho_hat) / gamma_hat
+        delta = max(0.0, min(1.0, kappa_hat / t))
+
+        # Compute shrunk covariance matrix
+        shrunk_cov = delta * F + (1 - delta) * S
+        return shrunk_cov, delta
--- Hacka/modulo/optimization.py
+++ Hacka/modulo/optimization.py
@@ -1,46 +1,107 @@
 import numpy as np
+import seaborn as sns
+import metrics 
+import pandas as pd
 
-def minimize_vol(rp, e_r, cov, eps = 0.0001, alpha= 0.01):
-    n =  e_r.shape[0]
-    dc = np.hstack((np.ones((n,1)), e_r))
-    #Primeiro Chute
-    w = (np.ones(n)*1/n).reshape(n, 1)#Carteira
-    w = w/sum(w)
-    l = -1*(np.linalg.inv(dc.T@dc)@dc.T@cov@w)#Multiplicadores de Lagrange
+class Markowitz:
     
-    #Calculando grad(c(x)) 
-    #Criando a Matriz A
-    def generate_A(Sigma, dc, n, delta1 = 0, delta2 =0.0):
-        a = np.hstack([2*Sigma+delta1*np.eye(len(w)), dc])
-        b = np.hstack([dc.T, np.zeros((2,2))-delta2*np.eye(2)])
-        return np.vstack((a, b))
-    
-    #Criando o vetor b.
-    def generate_b(Sigma, dc, e_r, l, w, rp):
-        a = 2*Sigma@w + dc@l
-        b = np.ones((1,n))@w - 1
-        c = e_r.T@w - rp
-        return -1*np.vstack((a, b, c))
-    
-    def grad(w, cov, l, dc):
-        return (2*cov@w+ dc@l)
-    
-    A = generate_A(cov, dc, n)
-    
-    while np.sum(grad(w, cov, l, dc)) > 2*eps:                                               #while max(np.abs((grad(w,cov, l, dc)))) > eps:
-        b = generate_b(cov, dc, e_r, l, w, rp)
-        #print("b: ",b, "\n")
-        dx = np.linalg.inv(A)@b
-        #print("dx: ", dx, "\n")
-        dw, dl = dx[0:n][:], dx[n:n+2][:]
-        w = w + alpha*dw
-        #print("w: \n", w, "\n")
-        l = l + alpha*dl
+    def __init__(self, er, cov, sim=100000):
+        self.er = er
+        self.cov = cov
+        self.carteiras =None
         
-    return w
+        self.points = []
+        n = er.shape[0]
+        self.carteiras = []
+        self.retornos = []
+        self.vols = []
+
+        for i in range(sim):
+            carteira = (np.random.random(n))
+            carteira = carteira/sum(carteira)
+            self.carteiras.append(carteira)
+            self.retornos.append(float(carteira.T@er))
+            self.vols.append(float(np.sqrt(carteira.T@cov@carteira)))
+        
+        self.carteiras = np.array(self.carteiras)
+        self.retornos = np.array(self.retornos)
+        self.vols = np.array(self.vols)
+        
+    
 
 
+    def minimize_ipm(self, rp, eps = 0.0001, alpha= 0.01):
 
+        n =  self.er.shape[0]
+        dc = np.hstack((np.ones((n,1)), self.er))
+        #Primeiro Chute
+        w = (np.ones(n)).reshape(n, 1)#Carteira
+ 
+        l = -1*(np.linalg.inv(dc.T@dc)@dc.T@self.cov@w)#Multiplicadores de Lagrange
+        
+        #Calculando grad(c(x)) 
+        #Criando a Matriz A
+        def generate_A(Sigma, dc, n):
+            a = np.hstack([2*Sigma, dc])
+            b = np.hstack([dc.T, np.zeros((2,2))])
+            return np.vstack((a, b))
+
+        #Criando o vetor b.
+        def generate_b(Sigma, dc, e_r, l, w, rp):
+            a = 2*Sigma@w + dc@l
+            b = np.ones((1,n))@w - 1
+            c = e_r.T@w - rp
+            return -1*np.vstack((a, b, c))
+
+        def grad(w, cov, l, dc):
+            return (2*cov@w+ dc@l)
+
+        A = generate_A(self.cov, dc, n)
+        i = 0
+        while (abs((w.T@self.er - rp + np.sum(w) - 1 )) > 2*eps): 
+            i = i+1
+            b = generate_b(self.cov, dc, self.er, l, w, rp)
+            dx = np.linalg.inv(A)@b
+       
+            dw, dl = dx[0:n][:], dx[n:n+2][:]
+            w = w + alpha*dw
+            
+            l = l + alpha*dl
+        return w
+    
+    def minimize_montecarlo(self, rp):
+        beg = np.min(self.vols)
+        end = np.max(self.vols)
+        
+        step = (end-beg)/1000       
+        ef_x =[]
+        ef_y = []
+        for i in range(1000):
+            x = (beg+i*step + beg+(i+1)*step)/2
+            try: 
+                y = np.max(self.retornos[np.logical_and((self.vols>=beg+i*step), (self.vols<=beg+(i+1)*step))])
+                ef_x.append(x)
+                ef_y.append(y)
+            except ValueError: 
+                pass
+        ef_y = np.array(ef_y)
+        i = np.argmin(abs(ef_y - rp))
+        
+       
+        return self.carteiras[self.retornos == ef_y[i]]
+            
+       
+          
+    def draw_ef(self, n_points = 100):
+        retornos = np.linspace(np.min(self.er), np.max(self.er), n_points)
+        carteiras = [self.minimize_montecarlo(i) for i in retornos]
+        
+        r = [float(metrics.portfolio_return(carteira.reshape(4,1), self.er)) for carteira in carteiras]
+        vol = [float(metrics.portfolio_vol(carteira.reshape(4,1), self.cov)) for carteira in carteiras]
+        
+        df = pd.DataFrame({"Retorno": r, "Vol":vol})
+        
+        return df.plot(x="Vol", y="Retorno", marker = ".", color="goldenrod", kind="scatter", figsize=(12,6))
 
 
 
